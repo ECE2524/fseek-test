45283f91b2141be28f0b2cf35e584badb1366b8a
Darren Maczka <dmaczka@vt.edu>
2013-12-17 10:23:45 -0500

    simplify example
    reading line by line is somewhat unnecessary to demonstrate the
    difference between a pipe and a redirect.

--- fdaccess.c
+++ fdaccess.c
@@ -15,17 +15,7 @@
 #include <err.h>
 
 int main() {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
 	FILE* fp = stdin;
-
-	while ((read = getline(&line, &len, fp)) != -1) {
-		printf("%s", line);
-	}
-	
-	free(line);
-	line = NULL;
 
 	if (fseek(fp, 0, SEEK_END) != 0) {
 		err(1, "(standard input)");
